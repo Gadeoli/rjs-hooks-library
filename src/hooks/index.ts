4e1f5b68860e2c2c22f794cc97c5a90cf89b89f4
Gabriel Morais <gabriel.moraisoliveira1@gmail.com>
2022-07-13 14:53:16 -0300

    remove duplicated hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,5 +4,4 @@ export { default as usePersistedState } from "./usePersistedState";
 export { default as usePrevious }       from "./usePrevious";
 export { default as useTimer }          from "./useTimer";
 export { default as useWindowSize }     from "./useWindowSize";
-export { default as useClickOutside }   from "./useClickOutside";
 export { default as useOnPressKey }     from "./useOnPressKey";
--- src/hooks/useClickOutside.types.ts
+++
@@ -1,3 +0,0 @@
-export interface useClickOutsideProps {
-    (ref: any, action: any) : void;
-}
