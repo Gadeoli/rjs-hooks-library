5f1394d04aca2d7f82e86fdee37609a6c55768a8
Gadeoli <gabriel.moraisoliveira1@gmail.com>
2025-11-07 14:29:46 -0300

    update useCabinet

--- src/hooks/useCabinet.types.ts
+++ src/hooks/useCabinet.types.ts
@@ -3,7 +3,10 @@ export interface useCabinetProps {
         drawers: Array<boolean>,
         reset: () => Array<boolean>,
         push: (n: number) => Array<boolean>,
-        pull: (n: number) => Array<boolean>,
+        pushNested: (n: number) => Array<boolean>,
+        pull: () => Array<boolean>,
+        pullNested: (n: number) => Array<boolean>,
         reverse: (n: number) => Array<boolean>,
+        manual: (n: Array<boolean>) => Array<boolean>
     }
 }
